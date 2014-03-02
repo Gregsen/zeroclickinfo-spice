e49a457c4426a7edd2591849f5314b109caaa9dc
Gregsen <gkneitschel@gmail.com>
2014-03-02 22:28:11 +0100

    perls special vars...

--- lib/DDG/Spice/Marvel.pm
+++ lib/DDG/Spice/Marvel.pm
@@ -17,7 +17,7 @@ triggers startend => 'Marvel';
 
 # helper function to read my dev keys from file
 sub getKey  {
-    my $file = @_;
+    my $file = $_;
     print $file;
     open my $fh, "<", $file or die $!;
     my $pass = <$fh>;
