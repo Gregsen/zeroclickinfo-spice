87fe1bd8258a103dd0551fba2748b05c962487d0
Gregsen <gkneitschel@gmail.com>
2014-03-03 21:51:20 +0100

    added handlebar template. tried to get apikeys working

--- share/spice/marvel/marvel.js
+++ share/spice/marvel/marvel.js
@@ -5,8 +5,9 @@ function ddg_spice_marvel (api_result) {
         data              : api_result,
         force_big_header  : true,
         header1           : api_result.name + ' (' + api_result.version + ')',
-            source_name       : "marvel.com", // More at ...
-            source_url        : 'http://marvel.com/universe/' + api_result.name,
-            template_normal   : 'marvel',
+        source_name       : "marvel.com", // More at ...
+        source_url        : 'http://marvel.com/universe/' + api_result.name,
+        force_no_fold     : true,
+        template_normal   : 'marvel'
             });
         }
