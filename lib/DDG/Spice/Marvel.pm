8012feebd144dbf109cfba9d3966a10cd0b4a89b
Gregsen <gkneitschel@gmail.com>
2014-03-05 13:11:27 +0100

    removed old code, fixed type

--- lib/DDG/Spice/Marvel.pm
+++ lib/DDG/Spice/Marvel.pm
@@ -15,25 +15,12 @@ attribution github => ['Gregsen', 'dd'];
 
 triggers start => 'marvel';
 
-# helper function to read my dev keys from file
-#sub getKey  {
-#    my ($file) = @_;
-#    print $file;
-#    open (my $fh, "<", $file) or die $!;
-#    my $pass = <$fh>;
-#    chomp($pass);
-#
-#    return $pass;
-#}
-#
-#my $privKey     = &getKey('privkey.txt');
-#my $pubKey      = &getKey('pubkey.txt');
 my $timeStamp   = time;
 my $secretKey   = '';#md5_hex($timeStamp.{{ENV{DDG_SPICE_MARVEL_PRIVKEY}}}.{{ENV{DDG_SPICE_MARVEL_PUBKEY}}});
 
 spice to => 'http://gateway.marvel.com/v1/public/characters?name=$1'
-            .'?ts=$timeStamp&apikey={{ENV{DDG_SPICE_MARVEL_PUBVKEY}}}'
-            .'&hash={{ENV{DDG_SPICE_MARVEL_PRIVKEY}}}';
+            .'?ts=$timeStamp&apikey={{ENV{DDG_SPICE_MARVEL_PUBKEY}}}'
+            .'&hash=$secretKey';
 spice wrap_jsonp_callback => 1;
 
 
